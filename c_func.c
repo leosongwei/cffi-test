b4746a57fe3987afbe2e6bdcebb5aa7f5d98eb33
SongWei <leo_songwei@outlook.com>
2018-08-12 21:22:05 +1000

    inline

--- c_func.c
+++ c_func.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <dlfcn.h>
 #include <time.h>
+
+int add1(int x)
+{
+  return x+1;
+}
 
 int main(void)
 {
-	int (*add1)(int x);
-	void *handle = dlopen("./libcint.so", RTLD_LAZY);
-
-	add1 = dlsym(handle, "add1");
-
-	clock_t time1 = clock();
 	int n = 0;
-	//(defparameter *test-scale* 100000000)
+	clock_t time1 = clock();
+
 	for(int i=0; i<100000000;i++){
 		n = add1(n);
 	}
--- main.lisp
+++ main.lisp
@@ -1,3 +1,5 @@
+(declaim (optimize (speed 3) (safety 0)))
+
 (ql:quickload 'cffi)
 
 (cffi:load-foreign-library "./libcint.so")
@@ -7,10 +9,12 @@
 (defun add1 (x)
   (declare (type (signed-byte 32) x))
   (+ x 1))
+(declaim (inline add1-inline))
+(defun add1-inline (x)
+  (declare (type (signed-byte 32) x))
+  (+ x 1))
 
 (defparameter *test-scale* 100000000)
-
-(declaim (optimize (speed 3) (safety 0)))
 
 (defun test-c ()
   (let ((n (floor (/ (- *test-scale*) 2))))
@@ -22,9 +26,31 @@
     (dotimes (i *test-scale*)
       (setf n (add1 n)))
     n))
+(defun test-inline ()
+  (let ((n (floor (/ (- *test-scale*) 2))))
+    (dotimes (i *test-scale*)
+      (setf n (add1-inline n)))
+    n))
 
+
+(format t "cffi~%")
 (time (test-c))
+
+(format t "funcall~%")
 (time (test))
+
+(format t "inline~%")
+(time (test-inline))
+
+(format t "C program, so, -O2 -ldl:~%~A"
+        (with-output-to-string (s)
+          (sb-ext:run-program "./cso" nil :output s)))
+
+(format t "~%")
+
+(format t "C program, func, -O2 -ldl:~%~A"
+        (with-output-to-string (s)
+          (sb-ext:run-program "./cfn" nil :output s)))
 
 (if (not (find-package 'swank))
     (sb-ext:exit))
